b9b4e1832fc657aab30b900294276995d871976c
Ammarclown <ammar.mohamed@student.giu-uni.de>
2022-05-14 22:11:45 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,5 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.listen(port, () => {
-console.log(`Server is running on port: ${port}`);
+console.log(`Server iss running on port: ${port}`);
 });
