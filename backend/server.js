719669c3b1f59ecc36bb0605e3e27aa050f85326
Ammarclown <ammar.mohamed@student.giu-uni.de>
2022-05-18 23:39:16 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config({path: './config.env'});
+
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-// get driver connection
-const dbo = require("./conn.js");
- 
+
+const uri = process.env.ATLAS;
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+console.log("MongoDB database connection established successfully");
+})
+
 app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server iss running on port: ${port}`);
+console.log(`Server is  running on port: ${port}`);
 });
+//hi
