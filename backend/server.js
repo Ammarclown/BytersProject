062f0db8b749ab4b4f043c77f5416474282e6f86
jumana-nounou <jumana.nounou@student.giu-uni.de>
2022-05-18 22:25:20 +0200

    Revert "Merge branch 'Branch1' of https://github.com/Ammarclown/BytersProject into Branch1"
    This reverts commit 0ded0514e057581c275411061d7985a84dfd0133, reversing
    changes made to 4f8344a5f6817a2153ab32ff4e1ec2709561f007.

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
- HEAD
 // get driver connection
 const dbo = require("./conn.js");
  
@@ -16,15 +15,4 @@ app.listen(port, () => {
  
   });
   console.log(`Server is running on port: ${port}`);
-
-
-const uri = process.env.ATLAS_URI;
-mongoose.connect('mongodb://localhost:27017/test');
-const connection = mongoose.connection;
-connection.once('open', () => {
-console.log("MongoDB database connection established successfully");
-})
-app.listen(port, () => {
-console.log(`Server is  running on port: ${port}`);
-
-});})
+});
