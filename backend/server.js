5a7ff26c57d6da233db3d78ac388783daec4f278
Ammarclown <ammar.mohamed@student.giu-uni.de>
2022-05-18 15:23:54 +0200

    setup

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config({path: './config.env'});
 
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect('mongodb://localhost:27017/test');
+
+const uri = process.env.ATLAS;
+mongoose.connect(uri);
 const connection = mongoose.connection;
 connection.once('open', () => {
 console.log("MongoDB database connection established successfully");
 })
-app.listen(port, () => {6
+
+app.listen(port, () => {
 console.log(`Server is  running on port: ${port}`);
 });
 //hi
