4f8344a5f6817a2153ab32ff4e1ec2709561f007
jumana-nounou <jumana.nounou@student.giu-uni.de>
2022-05-18 16:51:55 +0200

    mongos latest setup

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-
+const express = require("express");
 const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-
-const uri = process.env.ATLAS_URI;
-mongoose.connect('mongodb://localhost:27017/test');
-const connection = mongoose.connection;
-connection.once('open', () => {
-console.log("MongoDB database connection established successfully");
-})
-app.listen(port, () => {6
-console.log(`Server is  running on port: ${port}`);
+// get driver connection
+const dbo = require("./conn.js");
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
 });
-//hi
